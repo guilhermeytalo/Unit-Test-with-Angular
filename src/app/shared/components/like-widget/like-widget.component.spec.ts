15a832e16b81c5d9cce2cdd26ad41218688d12da
Guilherme Ytalo <guilherme.gramosa@totvs.com.br>
2021-12-25 19:54:40 -0300

    day 3 of tests

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,17 +1,47 @@
-import { TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+
 import { UniqueIdService } from "../../services/unique-id/unique-id.service";
 import { LikeWidgetComponent } from "./like-widget.component";
+import { LikeWidgetModule } from "./like-widget.module";
 
 describe(LikeWidgetComponent.name,() => {
+  let fixture: ComponentFixture<LikeWidgetComponent> = null;
   let component: LikeWidgetComponent = null;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LikeWidgetComponent]
+      imports: [LikeWidgetModule],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(LikeWidgetComponent);
+    component = fixture.componentInstance;
   });
 
-  it('', () => {
-
+  it('Should create component', () => {
+    expect(component).toBeTruthy();
   });
+
+  it('Should auto generate ID when id input property is missing', () => {
+    fixture.detectChanges();
+    expect(component.id).toBeTruthy();
+  });
+
+
+  it('Should NOT generate ID when id input property is present', () => {
+    const someID = 'someId';
+    component.id = someID;
+    fixture.detectChanges();
+    expect(component.id).toBe(someID);
+  });
+
+  it(`#${LikeWidgetComponent.prototype.like.name}
+    should trigger emission when call`, () => {
+      fixture.detectChanges();
+      component.liked.subscribe(() => {
+        expect(true).toBeTrue();
+      });
+      component.like();
+    }
+  );
 });
